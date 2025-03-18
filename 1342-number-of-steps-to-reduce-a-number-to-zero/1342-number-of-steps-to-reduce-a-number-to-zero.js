44b0c0cb1bf6e5fec002495694082cac5fad9e10
zeo <gayoungyeom@gmail.com>
2025-03-18 22:02:44 +0900

    Time: 0 ms (100%), Space: 53.5 MB (34.74%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -3,7 +3,22 @@
  * @return {number}
  */
 
- // O(n)
+// O(logN)
+// 주의: ((num & 1) === 0)과 (num & 1 === 0)은 다르다. -> 비교 연산이 비트 연산보다 우선순위가 높다.
+var numberOfSteps = function(num) {
+    let step = 0;
+
+    while(num !== 0) {
+        if((num & 1) === 0) num >>= 1;
+        else num -= 1;
+        
+        step++;
+    }
+
+    return step;
+};
+
+ // O(logN): 2로 나누니까
 var numberOfSteps = function(num) {
     let step = 0;
 
