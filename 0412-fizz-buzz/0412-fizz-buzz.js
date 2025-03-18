d4330a46c51e1fb814cafe1e887596b63b45da3d
zeo <gayoungyeom@gmail.com>
2025-03-18 21:21:43 +0900

    Time: 1 ms (67.95%), Space: 58.5 MB (14.15%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,15 +3,18 @@
  * @return {string[]}
  */
 
-//  O(n)
+// O(n)
+// 3과 5를 연속으로 보면, 15로 나누어 떨어질 때를 분기처리할 필요가 없다.
 var fizzBuzz = function(n) {
     const result = [];
 
-    for(let i = 0; i < n; i++){
-        if((i + 1) % 15 === 0) result.push("FizzBuzz");
-        else if((i + 1) % 3 === 0) result.push("Fizz");
-        else if((i + 1) % 5 === 0) result.push("Buzz");
-        else result.push(String(i + 1));
+    for(let i = 1; i <= n; i++){
+        let cur = '';
+        
+        if(i % 3 === 0) cur += "Fizz";
+        if(i % 5 === 0) cur += "Buzz";
+
+        result.push(cur || String(i));
     }
 
     return result;
