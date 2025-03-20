5d541f316798e4d61e28bf599097023d6fa1023c
zeo <gayoungyeom@gmail.com>
2025-03-20 22:36:43 +0900

    Time: 12 ms (83.63%), Space: 57.3 MB (36.69%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -5,22 +5,23 @@
  */
 
 // O(n)
-// var canConstruct = function(ransomNote, magazine) {
-//     const map = new Map();
+var canConstruct = function(ransomNote, magazine) {
+    const map = new Map();
     
-//     for(const m of magazine){
-//         map.set(m, map.has(m) ? map.get(m) + 1 : 1);
-//     }
+    for(const m of magazine){
+        map.set(m, map.has(m) ? map.get(m) + 1 : 1);
+    }
 
-//     for(const r of ransomNote){
-//         if(map.has(r) && map.get(r) !== 0) map.set(r, map.get(r) - 1);
-//         else return false;
-//     }
+    for(const r of ransomNote){
+        if(map.has(r) && map.get(r) !== 0) map.set(r, map.get(r) - 1);
+        else return false;
+    }
 
-//     return true;
-// };
+    return true;
+};
 
 
+// O(nm)
 // magazine의 값을 ransomNote에서 제거하는 방법. 
 // magazine을 다 보았을 때 ransomNote가 비어있다면 true 반환
 var canConstruct = function(ransomNote, magazine) {
