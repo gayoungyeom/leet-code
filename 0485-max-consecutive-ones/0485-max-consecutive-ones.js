dc965e228c0a0cad4b8c9e88b52606e71eec80d3
zeo <gayoungyeom@gmail.com>
2025-03-25 00:00:33 +0900

    Time: 2 ms (76.54%), Space: 60.1 MB (30.18%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -4,15 +4,18 @@
  */
 
 // O(n)
+// max계산 로직을 if(nums[i] === 1)안으로 옮기면 불필요한 계산을 줄일 수 있음.
 var findMaxConsecutiveOnes = function(nums) {
-    let result = nums[0];
-    let cnt = nums[0];
+    let result = 0;
+    let cnt = 0;
     
-    for(let i = 1; i < nums.length; i++){
-        if(nums[i] === 1) cnt++;
-        else cnt = 0;
-        
-        result = Math.max(result, cnt);
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === 1){
+            cnt++;
+            result = Math.max(result, cnt);
+        } else{
+            cnt = 0;
+        }
     }
     
     return result;
