af6432f91d636cef07b75a254f9e65f293ae6311
zeo <gayoungyeom@gmail.com>
2025-03-25 22:44:21 +0900

    Time: 0 ms (100%), Space: 56 MB (12.67%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -38,3 +38,24 @@ var merge = function(nums1, m, nums2, n) {
         }
     }
 };
+
+//개선: 불필요한 루프 제거
+// nums1은 이미 정렬된 상태이므로 남은 요소를 따로 복사할 필요가 없음
+// nums2 요소가 남아있을 때까지만 보면 된다는 의미 -> if(ni >= 0)
+var merge = function(nums1, m, nums2, n) {
+    let i = n + m - 1;
+    
+    let mi = m - 1;
+    let ni = n - 1;
+
+    while(ni >= 0){
+        if(mi >= 0 && nums1[mi] > nums2[ni]){
+            nums1[i] = nums1[mi];
+            mi--;
+        } else{
+            nums1[i] = nums2[ni];
+            ni--;
+        }
+        i--;
+    }
+};
