3ccc1d3240ae8260a60359aa60228fca6c0b8686
zeo <gayoungyeom@gmail.com>
2025-04-01 00:03:35 +0900

    Time: 0 ms (100%), Space: 54 MB (14.8%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 
- // O(n)
+// O(n)
 var removeElement = function(nums, val) {
     let left = 0;
     let right = nums.length - 1;
@@ -25,4 +25,18 @@ var removeElement = function(nums, val) {
     const isVal = valIdx !== -1;
 
     return isVal ? valIdx : nums.length; //nums에 val이 없는 경우도 고려하자
+};
+
+// O(n): val이 아닌 수 앞엑서부터 덮어쓰기
+var removeElement = function(nums, val) {
+    let k = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== val){
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    
+    return k;
 };
