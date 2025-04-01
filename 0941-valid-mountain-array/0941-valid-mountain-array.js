af76d2f4e024147e2c61a3d9cc5db99f85078d5f
zeo <gayoungyeom@gmail.com>
2025-04-01 22:51:14 +0900

    Time: 49 ms (64.85%), Space: 58.6 MB (36.9%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.js
+++ 0941-valid-mountain-array/0941-valid-mountain-array.js
@@ -17,45 +17,45 @@ var validMountainArray = function(arr) {
     return true;
 };
 
-// O(n): 하나의 for loop로 처리(peak 존재 여부를 파악)
-//  var validMountainArray = function(arr) {
-//     if(arr.length < 3) return false;
-//     if(arr[0] >= arr[1]) return false;
+// O(n): 하나의 for loop로 처리(peak 존재 여부를 파악) -> 성능 제일 좋음
+ var validMountainArray = function(arr) {
+    if(arr.length < 3) return false;
+    if(arr[0] >= arr[1]) return false;
 
-//     let isPeak = false;
+    let isPeak = false;
 
-//     for(let i = 1; i < arr.length - 1; i++){
-//         if(arr[i] > arr[i + 1]) isPeak = true;
+    for(let i = 1; i < arr.length - 1; i++){
+        if(arr[i] > arr[i + 1]) isPeak = true;
 
-//         if(!isPeak && (arr[i] >= arr[i + 1])) return false; //increasing 중인데 조건 out
-//         if(isPeak && (arr[i] <= arr[i + 1])) return false; //decreasing 중인데 조건 out
-//     }
+        if(!isPeak && (arr[i] >= arr[i + 1])) return false; //increasing 중인데 조건 out
+        if(isPeak && (arr[i] <= arr[i + 1])) return false; //decreasing 중인데 조건 out
+    }
 
-//     return isPeak;
-// };
+    return isPeak;
+};
 
-// // O(n)
-// var validMountainArray = function(arr) {
-//     let peak = -1;
+// O(n)
+var validMountainArray = function(arr) {
+    let peak = -1;
     
-//     //check increasing
-//     for(let i = 1; i < arr.length; i++){
-//         if(arr[i - 1] < arr[i]){
-//            peak = i; 
-//         } else{
-//             break;
-//         }
-//     }
+    //check increasing
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i - 1] < arr[i]){
+           peak = i; 
+        } else{
+            break;
+        }
+    }
 
-//     //only 내림차순 || only 오름차순
-//     if(peak === -1 || peak === arr.length - 1) return false;
+    //only 내림차순 || only 오름차순
+    if(peak === -1 || peak === arr.length - 1) return false;
     
-//     //check decreasing
-//     for(let i = peak; i < arr.length; i++){
-//         if(arr[i] <= arr[i + 1]){
-//             return false;
-//         }
-//     }
+    //check decreasing
+    for(let i = peak; i < arr.length; i++){
+        if(arr[i] <= arr[i + 1]){
+            return false;
+        }
+    }
     
-//     return true;
-// };
+    return true;
+};
