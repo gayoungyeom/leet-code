eeea4c21722d2586eaebd077a4edcf6b66599428
zeo <gayoungyeom@gmail.com>
2025-04-01 21:50:01 +0900

    Time: 1 ms (76.71%), Space: 52.7 MB (68.34%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -3,28 +3,32 @@
  * @return {boolean}
  */
 
-// O(n): set을 활용
+// O(n): set(hash talbe)을 활용
+// cf. 초기 set에 arr을 모두 넣고 비교하면 자기 자신을 확인하는 경우가 생긴다.
 var checkIfExist = function(arr) {
-    const set = new Set(arr);
+    const set = new Set();
 
     for(let i = 0; i < arr.length; i++){
-        if(set.has(arr[i] * 2) || ((arr[i] & 1) === 0) && set.has(arr[i] / 2)){
+        const num = arr[i];
+
+        if(set.has(num * 2) || ((num & 1) === 0) && set.has(num / 2)){
             return true;
         }
+        set.add(num); //지금까지 조건을 만족하는 숫자가 없는 경우 set에 추가
     }
     
     return false;
 };
 
 // O(n^2)
-var checkIfExist = function(arr) {
-    for(let i = 0; i < arr.length; i++){
-        for(let j = i + 1; j < arr.length; j++){
-            if((arr[i] === 2 * arr[j]) || (arr[j] === 2 * arr[i])){
-                return true;
-            }
-        }
-    }
+// var checkIfExist = function(arr) {
+//     for(let i = 0; i < arr.length; i++){
+//         for(let j = i + 1; j < arr.length; j++){
+//             if((arr[i] === 2 * arr[j]) || (arr[j] === 2 * arr[i])){
+//                 return true;
+//             }
+//         }
+//     }
     
-    return false;
-};
+//     return false;
+// };
