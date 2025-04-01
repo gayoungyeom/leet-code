8ed665d4f980b8091dd7de0757a9b315c50d7c2e
zeo <gayoungyeom@gmail.com>
2025-04-01 22:50:18 +0900

    Time: 33 ms (99.35%), Space: 58.6 MB (31.44%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.js
+++ 0941-valid-mountain-array/0941-valid-mountain-array.js
@@ -3,6 +3,23 @@
  * @return {boolean}
  */
 
+// O(n): 하나의 for loop로 처리(peak 존재 여부를 파악)
+ var validMountainArray = function(arr) {
+    if(arr.length < 3) return false;
+    if(arr[0] >= arr[1]) return false;
+
+    let isPeak = false;
+
+    for(let i = 1; i < arr.length - 1; i++){
+        if(arr[i] > arr[i + 1]) isPeak = true;
+
+        if(!isPeak && (arr[i] >= arr[i + 1])) return false; //increasing 중인데 조건 out
+        if(isPeak && (arr[i] <= arr[i + 1])) return false; //decreasing 중인데 조건 out
+    }
+
+    return isPeak;
+};
+
 // O(n)
 var validMountainArray = function(arr) {
     let peak = -1;
