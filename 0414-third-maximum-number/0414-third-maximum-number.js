7f815dc2ffcbcf83bc5fd736fef68057163776da
zeo <gayoungyeom@gmail.com>
2025-04-03 22:37:48 +0900

    Time: 4 ms (52.98%), Space: 58.4 MB (6.51%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.js
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -10,3 +10,26 @@ var thirdMax = function(nums) {
     
     return uniqueSortedArr.length < 3 ? uniqueSortedArr[0] : uniqueSortedArr[2];
 };
+
+// O(n)
+var thirdMax = function(nums) {
+    let first = -Infinity;
+    let second = -Infinity;
+    let third = -Infinity;
+
+    for(let i = 0; i < nums.length; i++){
+        //unique하지 않은 경우 pass
+        if(nums[i] === first || nums[i] === second || nums[i] === third) continue;
+
+        //max값으로 업데이트
+        if(nums[i] > first){
+            [first, second, third] = [nums[i], first, second]
+        } else if(nums[i] > second){
+            [second, third] = [nums[i], second]
+        } else if(nums[i] > third){
+            third = nums[i]
+        }
+    }
+
+    return third === -Infinity ? first : third;
+};
