4bebbbfd02d4793424d7ff39d30f7eb3520006d9
zeo <gayoungyeom@gmail.com>
2025-04-03 22:38:30 +0900

    Time: 4 ms (52.98%), Space: 56.7 MB (32.17%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.js
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -4,12 +4,12 @@
  */
 
 // O(nlogn)
-var thirdMax = function(nums) {
-    const set = new Set(nums);
-    const uniqueSortedArr = [...set.values()].sort((a, b) => b - a);
+// var thirdMax = function(nums) {
+//     const set = new Set(nums);
+//     const uniqueSortedArr = [...set.values()].sort((a, b) => b - a);
     
-    return uniqueSortedArr.length < 3 ? uniqueSortedArr[0] : uniqueSortedArr[2];
-};
+//     return uniqueSortedArr.length < 3 ? uniqueSortedArr[0] : uniqueSortedArr[2];
+// };
 
 // O(n)
 var thirdMax = function(nums) {
