6e2c119c69be8aeb50f41aa7735a578d491ebd32
zeo <gayoungyeom@gmail.com>
2025-04-14 23:07:30 +0900

    Time: 3 ms (79.6%), Space: 62.6 MB (39.85%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -7,3 +7,26 @@
 var sortedSquares = function(nums) {
     return nums.map(num => num * num).sort((a, b) => a - b);
 };
+
+// O(n) two pointer: 배열하나 만들고 뒤에서부터 큰 수를 채우기
+var sortedSquares = function(nums) {
+    const result = [...Array(nums.length).fill(0)];
+
+    let left = 0;
+    let right = nums.length - 1;
+    let i = right;
+
+    while(left <= right){
+        const leftNum = nums[left] * nums[left];
+        const rightNum = nums[right] * nums[right];
+
+        result[i] = Math.max(rightNum, leftNum);
+
+        if(leftNum <= rightNum) right--;
+        else left++;
+
+        i--;
+    }
+
+    return result;
+};
